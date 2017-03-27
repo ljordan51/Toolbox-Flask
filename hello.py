da478b3bd6ee4988c06e00bb2270cc84bc9dcf15
ljordan51 <ljordan51@gmail.com>
2017-03-27 15:17:02 -0400

    working on toolbox, next step is to send data from form and redirect user

--- hello.py
+++ hello.py
@@ -3,11 +3,20 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 
 if __name__ == '__main__':
     app.run()
