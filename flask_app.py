31ddda9eb9d5a5ee4f3286b45fd87d6c627441e3
ljordan51 <ljordan51@gmail.com>
2017-03-28 16:39:55 -0400

    turning in flask toolbox

--- flask_app.py
+++ flask_app.py
@@ -2,14 +2,24 @@
 Put your Flask app code here.
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    name = request.form['name']
+    age = request.form['age']
+    ninja = request.form['ninja']
+    if name and age and ninja:
+        return render_template('login.html', name=name, age=age, error=False)
+    else:
+        return render_template('login.html', error=True)
 
 
 if __name__ == '__main__':
